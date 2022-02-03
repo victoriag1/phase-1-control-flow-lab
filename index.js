7f2032131966813d77d981dde396193f403b1d7c
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-03 14:12:05 -0800

    commit tree ae094c9cd673ba23642db9b956257ee5f491633e

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sample) { 
+  if (sample <= 400) {
+    return "This one is on me!"
+  } else if (sample > 2000 && sample <2500) {
+    return "I will gladly take your thirty bucks."
+    }
+    else 
+    return "No can do."
+  }
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else 
+  return "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      case "not as generous":
+        return "Thank you."
+        default:
+          return "Bye."
+  }
 }
